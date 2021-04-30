e899f5eb23cd81cbfef9ce86c70ca57169f5f5ad
SlavaTVA <slava.tva83@gmail.com>
2021-04-30 14:40:25 +0300

    Add CodeClimate and configure Eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions, no-console */
+
 import { greeting } from '../src/cli.js';
-console.log("Welcome to the Brain Games!");
+
+console.log('Welcome to the Brain Games!');
 greeting();
