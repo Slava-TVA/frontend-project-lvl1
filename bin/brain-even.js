fca4e843bada0e7b1167ee978a1f04be373158f2
SlavaTVA <slava.tva83@gmail.com>
2021-05-01 16:33:52 +0300

    Add game Brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import greeting, { isEven } from '../src/index.js';
+
+greeting();
+isEven();
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+let name;
+
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+const isEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const num = Math.floor(Math.random() * 100);
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = (num % 2 === 0) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
+
+export { isEven };
