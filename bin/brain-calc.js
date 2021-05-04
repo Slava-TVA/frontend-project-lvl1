4168ce63ff72dd68e6c8ecd19c146bfdd81326d3
SlavaTVA <slava.tva83@gmail.com>
2021-05-04 13:37:50 +0300

    Add game Brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import greeting, { calc } from '../src/index.js';
+
+greeting();
+calc();
--- src/index.js
+++ src/index.js
@@ -28,4 +28,43 @@ const isEven = () => {
   }
 };
 
-export { isEven };
+const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const calcExp = (mathSymbol, a, b) => {
+  switch (mathSymbol) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
+  }
+};
+
+const calc = () => {
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = Math.floor(Math.random() * 100);
+    const num2 = Math.floor(Math.random() * 100);
+    const operators = ['+', '-', '*'];
+    const operatorIndex = getRandomInRange(0, 2);
+    const operator = operators[operatorIndex];
+    console.log(`Question: ${num1} ${operator} ${num2}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = String(calcExp(operator, num1, num2));
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
+
+export { isEven, calc };
